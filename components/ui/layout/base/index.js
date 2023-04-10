8a0ec9b79116de084306094e0a6fed35aff214e1
Shivam Raj <saivite@outlook.in>
2023-04-11 02:04:53 +0530

    load contract using Script

--- components/ui/layout/base/index.js
+++ components/ui/layout/base/index.js
@@ -1,18 +1,25 @@
 import { Web3Provider } from "@components/providers";
 import { Navbar, Footer } from "@components/ui/common";
+import Script from "next/script";
 
 export default function BaseLayout({ children }) {
   return (
-    <Web3Provider>
-      <div className="max-w-7xl mx-auto px-4">
-        {/*------ NAVBAR STARTS ------*/}
-        <Navbar />
-        {/*------ NAVBAR ENDS ------*/}
-        <div className="fit">{children}</div>
-      </div>
-      {/*------ FOOTER STARTS ------*/}
-      <Footer />
-      {/*------ FOOTER ENDS ------*/}
-    </Web3Provider>
+    <>
+      <Script
+        src="/js/truffle-contract.js"
+        strategy="beforeInteractive" /*this script should be loaded before others */
+      />
+      <Web3Provider>
+        <div className="max-w-7xl mx-auto px-4">
+          {/*------ NAVBAR STARTS ------*/}
+          <Navbar />
+          {/*------ NAVBAR ENDS ------*/}
+          <div className="fit">{children}</div>
+        </div>
+        {/*------ FOOTER STARTS ------*/}
+        <Footer />
+        {/*------ FOOTER ENDS ------*/}
+      </Web3Provider>
+    </>
   );
 }
--- utils/loadContract.js
+++ utils/loadContract.js
@@ -1,11 +1,9 @@
-import contract from "@truffle/contract";
-
 export const loadContract = async (name, provider) => {
   //we'll fetch CourseMarketplace.json file in public/contracts folder
   const res = await fetch(`/contracts/${name}.json`);
   const Artifact = await res.json();
-
-  const _contract = contract(Artifact);
+  //TruffleContract is globally available
+  const _contract = window.TruffleContract(Artifact);
   _contract.setProvider(provider);
 
   let deployedContract = null;
