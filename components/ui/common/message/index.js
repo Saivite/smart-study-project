57b44e82b48da9272276a29023598a11327638bd
Shivam Raj <saivite@outlook.in>
2023-04-12 12:01:13 +0530

    course state message displayed accordingly in individual course

--- components/ui/common/message/index.js
+++ components/ui/common/message/index.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
 
 const TYPES = {
-  warn: "indigo",
+  warn: "yellow",
   success: "green",
   danger: "red",
 };
 
-export default function Message({ children, type = "success" }) {
+export default function Message({ children, type }) {
   const [isDisplayed, setIsDisplayed] = useState(true);
   if (!isDisplayed) {
     return null;
@@ -15,12 +15,18 @@ export default function Message({ children, type = "success" }) {
   const messageType = TYPES[type];
 
   return (
-    <div className={`bg-${messageType}-100 rounded-xl mb-3`}>
+    <div
+      className={`bg-${messageType != null && messageType}-100 rounded-xl mb-3`}
+    >
       <div className="max-w-7xl mx-auto py-2 px-1">
         <div className="flex items-center justify-between flex-wrap">
           <div className="w-0 flex-1 flex items-center">
-            <div className={`ml-3 font-medium text-${messageType}-900`}>
-              <span className="hidden md:inline">{children}</span>
+            <div
+              className={`ml-3 font-medium text-${
+                messageType != null && messageType
+              }-900 `}
+            >
+              <span className={`hidden md:inline `}>{children}</span>
             </div>
           </div>
           <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
@@ -31,7 +37,9 @@ export default function Message({ children, type = "success" }) {
             >
               <span className="sr-only">Dismiss</span>
               <svg
-                className={`text-${messageType}-900 h-6 w-6 text-white-900`}
+                className={`text-${
+                  messageType != null && messageType
+                }-800 h-6 w-6 text-white-800`}
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -1,6 +1,6 @@
 //each course page is rendered by slug
 import { useAccount, useOwnedCourse } from "@components/hooks/web3";
-import { Modal } from "@components/ui/common";
+import { Message, Modal } from "@components/ui/common";
 import { CourseHero, Curriculum, Keypoints } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { getAllCourses } from "@content/courses/fetcher";
@@ -8,8 +8,8 @@ import { getAllCourses } from "@content/courses/fetcher";
 export default function Course({ course }) {
   const { account } = useAccount();
   const { ownedCourse } = useOwnedCourse(course, account.data);
-
-  console.log(ownedCourse);
+  const courseState = ownedCourse.data != null && ownedCourse.data.state;
+  // const courseState = "Activated";
 
   return (
     <>
@@ -24,15 +24,42 @@ export default function Course({ course }) {
         />
         {/*------ HERO ENDS ------*/}
       </div>
-
       {/*------ KEYPOINT STARTS ------*/}
       <Keypoints points={course.wsl} />
+      {courseState && (
+        <div className=" max-w-5xl mx-auto">
+          {courseState === "Purchased" && (
+            <Message type="warn">
+              Thank You for Purchasing the Course. Your learning is in the
+              process of activation. Process can take up to 24 hours.
+              <i className="block font-normal">
+                In case of any questions, please contact: saivite@outlook.in
+              </i>
+            </Message>
+          )}
+          {courseState === "Activated" && (
+            <Message type="success">
+              Instructor wishes you happy learning.
+              <i className="block font-normal">
+                In case of any questions, please contact: saivite@outlook.in
+              </i>
+            </Message>
+          )}
+          {courseState === "Deactivated" && (
+            <Message type="danger">
+              Course is deactivated due to discrepancy in order data. Access to
+              course is disabled.
+              <i className="block font-normal">
+                Please contact: saivite@outlook.in
+              </i>
+            </Message>
+          )}
+        </div>
+      )}
       {/*------ KEYPOINT ENDS ------*/}
-
       {/*------ LECTURES STARTS ------*/}
       <Curriculum locked={true} />
       {/*------ LECTURES ENDS ------*/}
-
       {/* MODAL STARTS */}
       <Modal />
       {/* MODAL ENDS */}
