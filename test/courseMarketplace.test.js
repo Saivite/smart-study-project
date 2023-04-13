c5e0702a3ed252ade6b09d59bdd790f11a30746d
Shivam Raj <saivite@outlook.in>
2023-04-13 22:55:52 +0530

    test for course data

--- test/courseMarketplace.test.js
+++ test/courseMarketplace.test.js
@@ -29,6 +29,7 @@ contract("CourseMarketplace", (accounts) => {
 
   //describe groups multiple tests
   describe("Purchasing a new course", () => {
+    let courseHash;
     before(async () => {
       //here we are working with abstraction of truffle so we dont need to write contract.methods... like in web3
       await _contract.purchaseCourse(courseId, proof, {
@@ -40,7 +41,7 @@ contract("CourseMarketplace", (accounts) => {
     //test 1 - if we purchase the course, we'll be able to readi it by index
     it("can get purchased course hash by index", async () => {
       const index = 0;
-      const courseHash = await _contract.getCourseHashAtIndex(index);
+      courseHash = await _contract.getCourseHashAtIndex(index);
       const expectedCourseHash = web3.utils.soliditySha3(
         {
           type: "bytes16",
@@ -55,6 +56,22 @@ contract("CourseMarketplace", (accounts) => {
         courseHash,
         expectedCourseHash,
         "Course Hash is not matching the hash of purchased course!"
+      );
+    });
+
+    it("should match the  data of the course purchased by buyer", async () => {
+      const expectedIndex = 0;
+      const expectedState = 0;
+      const course = await _contract.getCourseByHash(courseHash);
+      assert.equal(course.id, expectedIndex, "Course index should be 0");
+      //value is at which we have purchased the course
+      assert.equal(course.price, value, `Course price should be ${value}!`);
+      assert.equal(course.proof, proof, `Course proof should be ${proof}!`);
+      assert.equal(course.owner, buyer, `Course buyer should be ${buyer}!`);
+      assert.equal(
+        course.state,
+        expectedState,
+        `Course state should be ${expectedState}!`
       );
     });
   });
